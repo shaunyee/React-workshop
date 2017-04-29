13d2155411111e5a71662903e16315b44b819126
Shaun Yee <yeezy@Shauns-MacBook-Pro.local>
2017-04-29 11:54:48 -0700

    adds nested component of beers, adds new logo

---
+++ src/Beer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Beer extends Component {
+  render(){
+    return(
+      <div>
+        <ul>Racer 5 IPA</ul>
+        <ul>Pliney</ul>
+        <ul>Shock Top</ul>
+        <ul>Anchorsteam</ul>
+        <ul>Sam Adams</ul>
+      </div>
+    );
+  }
+}
+
+export default Beer
